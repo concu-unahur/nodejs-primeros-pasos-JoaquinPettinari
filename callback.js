239745ebf64189bd269adc4f2f10c09d2c369cad
JoaquinPettinari <joaquinpettinari@hotmail.com>
2020-03-03 20:42:49 -0300

    Falta el ultimo

--- callback.js
+++ callback.js
@@ -2,6 +2,41 @@ function siguiente(x, done) {
   done(x + 1)
 }
 
-siguiente(2, resultado => { 
-  console.log('Siguiente de 8 =>', resultado)
-})
+function doble(x, done){
+  done(x * 2)
+}
+
+
+function imprimir(resultado){
+  console.log('Resultado:', resultado)
+}
+
+
+siguiente(8, imprimir)
+
+doble(8, imprimir)
+
+doble(8, funcion)
+
+function funcion(numero){
+  siguiente(numero, imprimir)
+}
+
+siguiente(8, funcion2)
+
+function funcion2(numero){
+  doble(numero, imprimir)
+}
+
+
+function dobleDelSiguiente(x, done){
+  done(doble(siguiente))
+}
+
+
+dobleDelSiguiente(8, imprimir)
+
+
+
+
+
